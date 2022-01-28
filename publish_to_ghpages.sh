0e239684148db6ee3be18e5a959314967d288930
leyoubaloy <1942956063@qq.com>
2022-01-28 21:18:02 +0800

    更新

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -24,7 +24,11 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public
+touch CNAME
+echo "www.leyoubaloy.xyz" >> CNAME
+git add --all
+git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
 git push --all
