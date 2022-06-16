b058f6cdfb60fcffc0e69fbbba3e1247829a0915
leyoubaloy <1942956063@qq.com>
2022-06-17 00:52:28 +0800

    更新

---
+++ python脚本/替换成hugo图片.py
@@ -0,0 +1,15 @@
+import os
+import re
+path = "/Users/sunbaile/myBlog/content/posts"
+# path = "/Users/sunbaile/myBlog/python脚本"
+file = "浅记一下最近的恋爱片段～.md"
+
+with open(os.path.join(path, file),"r") as f:
+    all_text = f.read()
+    res = re.sub(r"!\[.*\]\((https:\/\/myblog.*)\)", '{{< figure src="\\1" >}}', all_text, 0, re.MULTILINE)
+    
+    
+with open(os.path.join(path, file),"w") as f:
+    f.write(res)
+
+
