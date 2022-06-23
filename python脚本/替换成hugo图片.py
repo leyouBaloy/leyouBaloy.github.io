acbb6421450fa734e873d5d65e0ea95638d513e4
leyoubaloy <1942956063@qq.com>
2022-06-23 13:50:02 +0800

    更新

--- python脚本/替换成hugo图片.py
+++ python脚本/替换成hugo图片.py
@@ -2,7 +2,7 @@ import os
 import re
 path = "/Users/sunbaile/myBlog/content/posts"
 # path = "/Users/sunbaile/myBlog/python脚本"
-file = "浅记一下最近的恋爱片段～.md"
+file = "组织一次成功的班级烧烤经验分享.md"
 
 with open(os.path.join(path, file),"r") as f:
     all_text = f.read()
